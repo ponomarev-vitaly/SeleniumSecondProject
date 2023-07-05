6d5330474fa9c412c88aa3e284a795e397be9157
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-05 11:08:19 +0300

    I added hooks in AmazonSearchTest @BeforeAll, @Test, @AfterAll. I modified the code and added it according to hooks.

--- src/main/java/AmazonSearchTest.java
+++ src/main/java/AmazonSearchTest.java
@@ -1,8 +1,59 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AmazonSearchTest {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
     @Test
     public void AmazonSearch(){
+        driver.get("https://amazon.com");
 
+        String searchPhrase = "iphone";
+
+        WebElement searchInput = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
+        searchInput.sendKeys(searchPhrase);
+        searchInput.sendKeys(Keys.ENTER);
+
+        List<String> actualItems = driver.findElements(By.cssSelector("[data-component-type='s-search-result'] h2.a-link-normal"))
+                .stream()
+                .map(element -> element.getText().toLowerCase() + element.getAttribute("href").toLowerCase())
+                .collect(Collectors.toList());
+        List<String> expectedItems = actualItems.stream()
+                .filter(item -> item.contains(searchPhrase))
+                .collect(Collectors.toList());
+
+        System.out.println(actualItems); // Print out actualItems and expectedItems into the console.
+        System.out.println("=============================>");
+        System.out.println(expectedItems);
+
+        Assertions.assertEquals(expectedItems, actualItems);
+    }
+    @AfterAll
+    public static void tearDownDriver(){
+        driver.quit();
     }
 }
