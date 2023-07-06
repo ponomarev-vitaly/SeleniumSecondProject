b4dfb5874f6acb4ecca7af09b3d30488a6f7661f
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-07 00:04:35 +0300

    I modified the code. I added page object and run the test successfully.

--- src/main/java/AmazonSearchTest.java
+++ src/main/java/AmazonSearchTest.java
@@ -9,13 +9,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.impl.HomePage;
+import pages.impl.SearchResultsPage;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class AmazonSearchTest {
-
+    private static final String searchPhrase = "iphone";
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeAll
     public static void setUpDriver(){
@@ -27,33 +33,51 @@ public class AmazonSearchTest {
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+    @BeforeAll
+    public static void setUpWait(){
+        wait = new WebDriverWait(driver, 15);
+    }
+
+    private static void switchOffImplicitWait(){
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     }
     @Test
     public void AmazonSearch(){
         driver.get("https://amazon.com");
 
-        String searchPhrase = "iphone";
+        HomePage homePage = new HomePage(driver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
 
-        WebElement searchInput = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
-        searchInput.sendKeys(searchPhrase);
-        searchInput.sendKeys(Keys.ENTER);
+        homePage.performSearch(searchPhrase);
 
-        List<String> actualItems = driver.findElements(By.cssSelector("[data-component-type='s-search-result'] h2.a-link-normal"))
-                .stream()
-                .map(element -> element.getText().toLowerCase() + element.getAttribute("href").toLowerCase())
-                .collect(Collectors.toList());
-        List<String> expectedItems = actualItems.stream()
-                .filter(item -> item.contains(searchPhrase))
-                .collect(Collectors.toList());
+        List<String> actualItems = searchResultsPage.searchResultsItemsText();
+        List<String> expectedItems = searchResultsPage.searchResultsItemsWithText(searchPhrase);
+
+        Assertions.assertEquals(expectedItems, actualItems);
+
+//        WebElement searchInput = driver.findElement(By.cssSelector("#twotabsearchtextbox"));
+//        searchInput.sendKeys(searchPhrase);
+//        searchInput.sendKeys(Keys.ENTER);
+//
+//        List<String> actualItems = driver.findElements(By.cssSelector("[data-component-type='s-search-result'] h2.a-link-normal"))
+//                .stream()
+//                .map(element -> element.getText().toLowerCase() + element.getAttribute("href").toLowerCase())
+//                .collect(Collectors.toList());
+//        List<String> expectedItems = actualItems.stream()
+//                .filter(item -> item.contains(searchPhrase))
+//                .collect(Collectors.toList());
 
 //        System.out.println(actualItems); // Print out actualItems and expectedItems into the console.
 //        System.out.println("=============================>");
 //        System.out.println(expectedItems);
 
-        Assertions.assertEquals(expectedItems, actualItems);
+//        Assertions.assertEquals(expectedItems, actualItems);
     }
     @AfterAll
     public static void tearDownDriver(){
+        System.out.println(LocalDateTime.now());
         driver.quit();
     }
 }
--- src/main/java/pages/impl/HomePage.java
+++ src/main/java/pages/impl/HomePage.java
@@ -1,4 +1,20 @@
 package pages.impl;
 
-public class HomePage {
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends WebPage {
+    @FindBy(css = "#twotabsearchtextbox")
+    private WebElement searchInput;
+
+    public HomePage(WebDriver driver){
+        super(driver);
+    }
+
+    public void performSearch(String searchPhrase){
+        searchInput.sendKeys(searchPhrase);
+        searchInput.sendKeys(Keys.ENTER);
+    }
 }
