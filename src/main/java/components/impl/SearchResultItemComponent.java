c65273ce9e5c9e4e8282f68f23f79a99ed8a4d34
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-20 02:32:44 +0300

    I modified the code and run the test successfully.

--- src/main/java/components/impl/SearchResultItemComponent.java
+++ src/main/java/components/impl/SearchResultItemComponent.java
@@ -6,18 +6,22 @@ import org.openqa.selenium.WebElement;
 
 public class SearchResultItemComponent extends WebComponent {
     private static final By TITLE_SELECTOR = By.cssSelector("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-size-medium a-color-base a-text-normal']");
-    private static final By DESCRIPTION_SELECTOR = By.cssSelector("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-truncate-cut']");
+//    private static final By DESCRIPTION_SELECTOR = By.cssSelector("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-truncate-cut']");
 
     public SearchResultItemComponent(WebElement rootElement) {
         super(rootElement);
     }
 
     public boolean containsSearchPhrase(String searchPhrase){
-        return containsSearchPhraseIgnoringCase(searchPhrase, TITLE_SELECTOR)
-                || containsSearchPhraseIgnoringCase(searchPhrase, DESCRIPTION_SELECTOR);
+        WebElement title = findElement(TITLE_SELECTOR);
+        return containsText(title.getText(), searchPhrase) || containsText(title.getAttribute("href"), searchPhrase);
+//        return containsSearchPhraseIgnoringCase(searchPhrase, TITLE_SELECTOR)
+//                || containsSearchPhraseIgnoringCase(searchPhrase, DESCRIPTION_SELECTOR);
     }
 
-    private boolean containsSearchPhraseIgnoringCase(String searchPhrase, By selector){
-        return findElement(selector).getText().toLowerCase().contains(searchPhrase);
-    }
+    private boolean containsText(String text, String searchPhrase){return text.toLowerCase().contains(searchPhrase);}
+
+//    private boolean containsSearchPhraseIgnoringCase(String searchPhrase, By selector){
+//        return findElement(selector).getText().toLowerCase().contains(searchPhrase);
+//    }
 }
