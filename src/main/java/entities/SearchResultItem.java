23ed5f473b4e08722acb7dbf81caa68e6e802a09
Vitaly Ponomarev <vp211082@gmail.com>
2023-07-21 00:58:39 +0300

    I created entities folder and added SearchResultItem java class in it.

---
+++ src/main/java/entities/SearchResultItem.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class SearchResultItem {
+    private final String title;
+
+    public SearchResultItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
